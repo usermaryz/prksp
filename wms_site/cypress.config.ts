5a779e56b566c88cfe21422c725e88c0a263d38d
Mary <zemlyanskayamariaa@gmail.com>
2026-04-26 02:31:41 +0300

    chore(frontend): TypeScript, тесты и форматирование

---
+++ wms_site/cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    component: {
+        devServer: {
+            framework: 'react',
+            bundler: 'webpack',
+            webpackConfig: require('./webpack.config.js')
+        },
+        specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/component.tsx',
+        indexHtmlFile: 'cypress/support/component-index.html'
+    },
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/e2e.ts',
+        setupNodeEvents(on, config) {
+            const webpack = require('@cypress/webpack-preprocessor');
+            on('file:preprocessor', webpack({ webpackOptions: require('./cypress/webpack.config.js') }));
+            return config;
+        }
+    },
+}); 
---
+++ wms_site/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+}; 
